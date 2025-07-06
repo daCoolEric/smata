86a64fc0d153e0ca85b546d7608a176ee02a39c6
Dacooleric <dacooleric@gmail.com>
2025-07-06 14:08:04 +0000

    hardcoded env details

--- app/config/supabaseConfig.js
+++ app/config/supabaseConfig.js
@@ -1,8 +1,12 @@
 // app/config/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://iwxvasuhwjcdoavjuuvs.supabase.co";
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3eHZhc3Vod2pjZG9hdmp1dXZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NjM2MjQsImV4cCI6MjA2NjEzOTYyNH0.OXl96l2oB9ruoTlUqQnCUu2uBC0P8b0Dvy4X0kMpuvo";
 
 // Only throw error in development or if completely missing
 if (
--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["pdfjs-dist"],
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   serverExternalPackages: ["@supabase/supabase-js"],
 };
 
